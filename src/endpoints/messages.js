d5d23f456a3f678699334dfb5d75131d111e6552
mahendra varma <bsmv.08@gmail.com>
2024-05-30 08:29:19 +0530

    one-one message trial

--- src/endpoints/messages.js
+++ src/endpoints/messages.js
@@ -5,14 +5,17 @@ const messages = require('../schemas/messages')
 router.get('/messages/:currentUser/:selectedUser', async(req, res) => {
     console.log(req.params);
     const currentUser = req.params.currentUser
-    const selectedUSer = req.params.selectedUser
+    const selectedUser = req.params.selectedUser
     console.log(currentUser,"currentUSer");
     const message = await messages.find({
-        sender:currentUser,
-        recipient: selectedUSer
-    }).sort({createdAt:-1})
+        $and: [
+          { $or: [{ sender: currentUser }, { recipient: currentUser }] },
+          { $or: [{ sender: selectedUser }, { recipient: selectedUser }] },
+        ]
+      }).sort({ createdAt: -1 });
+      
 
-    console.log(messages,"messagesss")
+    console.log(message,"messagesss")
     if(messages?.length){
         res.send({message:'Fetching messages was successful',ok:true,messages:message}); 
       }
