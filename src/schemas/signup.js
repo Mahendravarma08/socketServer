a3181303d38697ad8d8cc6ef6fb3bc8e1f737ed2
mahendra varma <bsmv.08@gmail.com>
2024-09-01 14:10:48 +0530

    Chages

--- src/schemas/signup.js
+++ src/schemas/signup.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const signUpSchema = new mongoose.Schema({
     userName:{type:String,required:true,unique:true},
-    email:{type:String,required:true},
     password:{type:String,required:true},
     // status : {type:Number,required:true}
     // socketId: { type: String,default:null }
